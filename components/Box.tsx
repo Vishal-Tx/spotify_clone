2411a4ca9b128abe695664baa4a052c6350b92c5
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-07-22 18:19:45 +0530

    use SidebarItem

--- components/Box.tsx
+++ components/Box.tsx
@@ -7,7 +7,13 @@ interface BoxProps {
 }
 
 const Box: React.FC<BoxProps> = ({ children, className }) => {
-  return <div className={twMerge("bg-neutral-900 rounded-lg h-fit w-full", className)}>{children}</div>;
+  return (
+    <div
+      className={twMerge("bg-neutral-900 rounded-lg h-fit w-full", className)}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default Box;
